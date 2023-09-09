a84a5aaf5882c7f122071ed1ad04d2474f21074a
fummicc1 <fumiya.tennis1@gmail.com>
2023-09-09 21:33:32 +0900

    fix: update method was not working.

--- Sources/RealmSwiftMacroMacros/RealmModelMacro.swift
+++ Sources/RealmSwiftMacroMacros/RealmModelMacro.swift
@@ -103,9 +103,15 @@ public static func create(\(raw: parameters)) async throws -> \(raw: className) 
                         return DeclSyntax("")
                     }
                     let decl: DeclSyntax = """
+let dict: [String: Any] = [:]
 if let \(raw: memberName) {
-    self.\(raw: memberName) = \(raw: memberName)
+    dict["\(raw: memberName)"] = \(raw: memberName)
 }
+realm.create(
+    \(raw: className).self,
+    value: dict,
+    update: .modified
+)
 """
                     return decl
                 }
